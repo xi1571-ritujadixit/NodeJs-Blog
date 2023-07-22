b8e478a313e5844c53839e1d30c0b2835df2a6c9
Rituja <rdixit@hagroup.com>
2023-07-22 14:18:16 +0530

    Part-3

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // inside app.js, we are going to make the web server which we are going to visit from the browser.
 require("dotenv").config();
 
+// making express server
 const express = require("express");
 const expressLayout = require("express-ejs-layouts");
 
@@ -9,6 +10,7 @@ const app = express();
 // 5000 port is for local use.
 const PORT = 5000 || process.env.PORT;
 
+// to use the public folder (contains css, imgs, js)
 app.use(express.static("public"));
 
 // Templating engine
@@ -16,6 +18,7 @@ app.use(expressLayout);
 app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
+// for all the routes
 app.use("/", require("./server/routes/main"));
 
 app.listen(PORT, () => {
--- server/routes/main.js
+++ server/routes/main.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 // routes
 router.get("", (req, res) => {
+    // to send some kind of data to the rendered page
     const locals = {
         title: "NodeJs Blog",
         description: "Simple Blog created with NodeJs, Express & MongoDb",
@@ -13,4 +14,5 @@ router.get("/about", (req, res) => {
     res.render("about");
 });
 
+// exporting router
 module.exports = router;
